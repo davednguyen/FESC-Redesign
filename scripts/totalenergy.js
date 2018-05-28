fa4a29e71dc6cde6941035c98fe7a5c538ae599b
David Nguyen <david.nguyen@smartsheet.com>
2018-05-28 12:09:34 -0700

    update css files

--- scripts/totalenergy.js
+++ scripts/totalenergy.js
@@ -1,13 +1,13 @@
-function displayChart(){
-    var displayInfo = document.getElementById("totalenergypage").style.display;
-    if(displayInfo === "inline"){
-        document.getElementById("totalenergypage").style.display = "none";
-        document.getElementById("displayButton").innerHTML = "Show energy chart";
-    }else{
-        document.getElementById("totalenergypage").style.display = "inline";
-        document.getElementById("displayButton").innerHTML = "Hide energy chart";
-    }  
-};
+// function displayChart(){
+//     var displayInfo = document.getElementById("totalenergypage").style.display;
+//     if(displayInfo === "inline"){
+//         document.getElementById("totalenergypage").style.display = "none";
+//         document.getElementById("displayButton").innerHTML = "Show energy chart";
+//     }else{
+//         document.getElementById("totalenergypage").style.display = "inline";
+//         document.getElementById("displayButton").innerHTML = "Hide energy chart";
+//     }  
+// };
 
 var totalEngergyButton = new Vue({
     el:'#energy',
