07f4cd8e15332c44ad314d19d756e466a6d19960
David Nguyen <david.nguyen@smartsheet.com>
2018-05-28 12:03:22 -0700

    updates index.html page and update using vue.js framework for the charge page

---
+++ scripts/totalenergy.js
@@ -0,0 +1,29 @@
+function displayChart(){
+    var displayInfo = document.getElementById("totalenergypage").style.display;
+    if(displayInfo === "inline"){
+        document.getElementById("totalenergypage").style.display = "none";
+        document.getElementById("displayButton").innerHTML = "Show energy chart";
+    }else{
+        document.getElementById("totalenergypage").style.display = "inline";
+        document.getElementById("displayButton").innerHTML = "Hide energy chart";
+    }  
+};
+
+var totalEngergyButton = new Vue({
+    el:'#energy',
+    data:{
+        name:'veu.js'
+    },
+    methods:{
+        displayChart:function(){
+            var displayInfo = document.getElementById("totalenergypage").style.display;
+            if(displayInfo === "inline"){
+                document.getElementById("totalenergypage").style.display = "none";
+                document.getElementById("displayButton").innerHTML = "Show energy chart";
+            }else{
+                document.getElementById("totalenergypage").style.display = "inline";
+                document.getElementById("displayButton").innerHTML = "Hide energy chart";
+            }  
+        }
+    }
+});
